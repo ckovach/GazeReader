ce5832e99f484b27a78da1b22e285533cdd91d4e
Christopher K. Kovach <ckovach@users.noreply.github.com>
2011-11-22 02:24:35 +0000

    add polynomial to ordinalReg

--- 0.1/ordinalReg.m
+++ 0.1/ordinalReg.m
@@ -1,6 +1,7 @@
 
 function Rout = ordinalReg(X,noptions,varargin)
 
+
 % Create design matrix and response vector for an adjacent-categories
 % ordinal logit (see Agresti 2010, chapter 3 & 4) for use with mnlfit.
 %
@@ -10,13 +11,21 @@ function Rout = ordinalReg(X,noptions,varargin)
 %
 %          where bi is some baseline category.
 %
-%  [Xout,Yout] = ordinalReg(Xin,Yin,noptions);
+%  Rout = ordinalReg(Xin,Yin,noptions,'params',value);
 %
 %  Xin is an NxK matrix containing the indepenent variable as rows. 
 %
+%  Rout is a GazeReader regressor array with intercepts in the first
+%  element, and the ordinal effect in the second. 
+%
 %  'noptions' is the total number of response categories which is either a
 %  scalar if the number of options is the same for each row (as is
-%  typically the case) or a vector otherwise. 
+%  typically the case) or a column vector otherwise. 
+%  
+%  'noptions' can also be a  row vector. If so, then each column 
+%  corresponds to a separate rating scale. Interaction terms between rating
+%  scales are contained in the third element of Rout.
+%  
 %
 %  The model is implemented as a single multinomial logit.
 %  
@@ -47,7 +56,7 @@ cdi = 0;
 label = 'covariates';
 incl_intcpt = true;
 index_fun = [];
-
+poly_intcpt = 0;
 i = 1;
 %%%% Key words are described below %%%%
 while i <= length(varargin)
@@ -65,6 +74,12 @@ while i <= length(varargin)
         case 'intercepts'
             incl_intcpt =  varargin{i+1};
             i = i+1;
+        case 'poly_intercept' %%% Use a polynomial of the given order
+                              %%% for the intercepts rather than separate
+                              %%% intercepts for each category
+            poly_intcpt =  varargin{i+1};
+            i = i+1;
+            incl_intcpt = false;
         case 'index_fun'  %%% Alternative transformation on indices for the intercept term
             indxfun =  varargin{i+1};
             incl_intcpt = false;
@@ -79,7 +94,7 @@ end
 
 N = size(X,1);
 
-if length(noptions) == 1
+if size(noptions,1) == 1
     noptions = repmat(noptions,N,1);
 end
 
@@ -87,28 +102,44 @@ end
 %%% Create a vector that expands each row of the design matrix to equal the
 %%% number of options.
 
-endind = zeros(sum(noptions),1);
-trend =cumsum( noptions(1:end));
-endind(trend) = noptions;
+totnopt = prod(noptions,2);
+endind = zeros(sum(totnopt),1);
+trend =cumsum( totnopt(1:end));
+endind(trend) = totnopt;
 expand = cumsum([1;endind(1:end-1)~=0]);
 
 %%% Create the GazeReader regressor for X
-RX = makeregressor(X(expand,:),'noptions',noptions,'label','X');
+RX = makeregressor(X(expand,:),'noptions',totnopt,'label','X');
 
 
-optn = [1:sum(noptions)]' - cumsum([0;endind(1:end-1)]); %option number
+optn = [1:sum(totnopt)]' - cumsum([0;endind(1:end-1)]); %option number
+if size(noptions,2) < 2
+    indx = optn;
+else
+    
+    %%% Note that noptions must be the same on every trial.
+    %%% Variable noptions over trials is not implemented here.
+    
+    indx = repmat(crossn(noptions(1,:)),size(noptions,1),1);
+    
+end
 
 if incl_intcpt
     %%% Create dummy variables for the options excluding the baseline
     RF = fact2reg(optn,'center',false,'ignore',optn == baseline_index,...
-                  'noptions',noptions,'label','intercepts','codeincr',cdi); cdi = cdi+1;
+                  'noptions',totnopt,'label','intercepts','codeincr',cdi); cdi = cdi+1;
+elseif poly_intcpt > 0 
+        
+    %%% Create polynomial basis functions over the indices of options
+       RF = buildpolyreg(indx,poly_intcpt,'codeincr',cdi,'noptions',totnopt,'label','intercepts poly','codeincr',cdi); cdi = cdi+1;
+       
 elseif ~isempty(indxfun)
-    RF = makeregressor(indxfun(optn),'codeincr',cdi,'noptions',noptions,'label','intercepts','codeincr',cdi); cdi = cdi+1;
+    for i = 1:size(indx,2)
+       RF(i) = makeregressor(indxfun(indx(:,i)),'codeincr',cdi,'noptions',totnopt,'label',sprintf('intercepts %i',i),'codeincr',cdi); cdi = cdi+1;
+    end
 else
     RF = [];
 end
-
-
 
 
 %%% Compute the interaction with option number. This is equivalent to the 
@@ -125,13 +156,73 @@ end
 
 
 %%% Regressor for the option number (used
-Rnopt = makeregressor(optn-1,'noptions',noptions,'label','order');
+Rnopt = makeregressor(indx-1,'noptions',totnopt,'label','order');
 
+if size(indx,2) > 1
+    Rnopt = split(Rnopt);
+end
 %%% INteraction term
 Rintxn = interaction(Rnopt,RX,'codeincr',cdi);cdi = cdi+1;
 
+if length(Rnopt) > 1
+    Rintint = interaction(Rnopt,'codeincr',cdi);cdi = cdi+1;
+else
+    Rintint = [];
+end
 %%% Array of output regressors
-Rout = [RF,Rintxn]; 
+Rout = [RF,Rintxn, Rintint]; 
 
 
+
+
+
+function tab = crossn(N,full)
+
+% tab = crossn(N)
+%
+% Creates a matrix whose rows contain all combinations of integers
+% 1:N(1),1:N(2),....
+%
+% For example, if N is a 2 X 1 vector of the lengths of 2 vectors, x1 and x2,
+% then crossn creates a  N(1)*N(2) X 2 matrix which contains every pair of
+% indices into x1 and x2.
+%
+%
+% tab = crossn(N,0)
+%
+% Returns only combinations that are unique under permutation.
+
+
+% C. Kovach 2010
+
+
+if nargin < 2 || isempty(full)
+    full = true;
+end
+
+
+if length(N) == 1    
+    tab = (1:N)';
+    return
+end
+    
+cn = crossn(N(2:end),full);
+    
+tab = cat(2,kron((1:N(1))',ones(size(cn,1),1)), repmat(cn,N(1),1));    
+    
+if ~full %Discard permutations of the same vector
+    
+%    [sqi,sqj]  = meshgrid(1:N(1),1:size(cn,1)); 
+%    
+%    keep = sqi <= sqj | sqi > size(cn,1);
+   
+   cols = 1:size(tab,2);
+   
+   [mn,mni] = min(max(tab));        
+
+   keep = all(repmat(tab(:,mni),1,length(cols)-1) - tab(:,setdiff(cols,mni)) <=0,2);  
+   
+   tab = tab(keep,:);
+
+end
 
